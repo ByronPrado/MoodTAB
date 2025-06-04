71cb983f19a0cf6e316030584ddb14d5b9342bfc
ByronPrado <byron.prado@sansano.usm.cl>
2025-06-04 01:34:46 -0400

    Prueba Conexion Internet
    Probamos la funcion de enviar un archivo, esto solo si se cuenta con conexión a internet. actualmente es el usuario quien hace el envio, pronto a cambiar jeje

--- MoodTAB/MauiProgram.cs
+++ MoodTAB/MauiProgram.cs
@@ -19,6 +19,7 @@ public static class MauiProgram
 
 #if DEBUG
 		builder.Logging.AddDebug();
+		builder.Services.AddSingleton(Connectivity.Current);
 		builder.Services.AddSingleton<ViewModel.MainViewModel>();
 		builder.Services.AddSingleton<MainPage>();
 
--- MoodTAB/Vistas/TestPage.xaml.cs
+++ MoodTAB/Vistas/TestPage.xaml.cs
@@ -1,9 +1,59 @@
 namespace MoodTAB.Vistas;
 
+using Microsoft.Maui.ApplicationModel; 
+using Microsoft.Maui.ApplicationModel.DataTransfer;
+
+
+
 public partial class TestPage : ContentPage
 {
+	public string Nombre { get; set; } = "Test Page";
+	IConnectivity connectivity;
 	public TestPage()
 	{
 		InitializeComponent();
+		NombreLabel.Text = Nombre; // Muestra el valor inicial
+		connectivity = Connectivity.Current;
+	}
+
+	public static string DatabasePath
+	{
+		get
+		{
+			var path = FileSystem.AppDataDirectory;
+			return Path.Combine(path, "MoodTAB.db3");
+		}
+	}
+	public  void OnButtonClicked(object sender, EventArgs e){
+		if (connectivity.NetworkAccess != NetworkAccess.Internet)
+		{
+			DisplayAlert("Error", "No hay conexión a Internet.", "OK");
+			return;
+		}
+		NombreLabel.Text = DatabasePath; // Cambia el texto al presionar el botón
+		Clipboard.SetTextAsync(NombreLabel.Text); // Copia al portapapeles
+		DisplayAlert("Copiado", "La dirección se copió al portapapeles.", "OK");
+		try
+		{
+			var file = DatabasePath;
+			if (!File.Exists(file))
+			{
+				DisplayAlert("Error", "La base de datos no existe.", "OK");
+				return;
+			}
+
+			Share.RequestAsync(new ShareFileRequest
+			{
+				Title = "Compartir archivo SQLite",
+				File = new ShareFile(file)
+			});
+		}
+		catch (Exception ex)
+		{
+			DisplayAlert("Error", $"No se pudo compartir el archivo: {ex.Message}", "OK");
+		}
 	}
 }
+
+
+
