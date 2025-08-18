0c1152535efe831dc6120ef0e5bbd14e01b12767
FranciscoMunozP <francisco.munozpe@usm.cl>
2025-08-17 21:23:36 -0400

    Corrección

--- MoodTAB/obj/Debug/net8.0-android/MoodTAB.AssemblyInfo.cs
+++ MoodTAB/obj/Debug/net8.0-android/MoodTAB.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MoodTAB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+2d59b13ef15ef85e306313dee1458a4199374e96")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+0d74f306f14c89022bf7d3a4c8b43d4981e9c69c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MoodTAB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MoodTAB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
